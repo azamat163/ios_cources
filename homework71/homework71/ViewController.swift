ea1736ebb3bd4b82bb59f9138caf2427d2a16737
Agataev Azamat <a.agataev@anytimecar.ru>
2020-01-28 16:42:27 +0300

    add homework7

---
+++ homework71/homework71/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homework71
+//
+//  Created by Azamat on 28.01.2020.
+//  Copyright © 2020 Azamat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
